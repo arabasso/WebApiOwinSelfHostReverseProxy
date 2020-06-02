cd3dd0e5d748b9b5aaabd4868413b5d24ab8504d
Raphael Basso <arabasso@yahoo.com.br>
2020-06-02 16:43:15 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiOwinSelfHostReverseProxy/MessageHandlers/ProxyDelegatingHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApiOwinSelfHostReverseProxy.MessageHandlers
+{
+    public class ProxyDelegatingHandler :
+            DelegatingHandler
+    {
+        private readonly HttpClient _client;
+        private readonly Uri _uri;
+
+        public ProxyDelegatingHandler(
+            string baseAddress,
+            HttpClient client)
+        {
+            _client = client;
+            _uri = new Uri(baseAddress);
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var context = request.GetOwinContext();
+
+            request.RequestUri = new Uri(_uri, request.RequestUri.LocalPath);
+
+            Console.WriteLine(request.RequestUri);
+
+            request.Headers.Add("X-Forwarded-For", context.Request.RemoteIpAddress);
+            request.Headers.Add("X-Real-IP", context.Request.RemoteIpAddress);
+            request.Headers.Add("X-Forwarded-Proto", _uri.Scheme);
+
+            request.Headers.Host = _uri.Host;
+
+            var ct = request.Content.Headers.ContentType;
+
+            if (request.Method == HttpMethod.Get)
+            {
+                request.Content = null;
+            }
+
+            return await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        }
+    }
+}
---
+++ WebApiOwinSelfHostReverseProxy/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+using WebApiOwinSelfHostReverseProxy.MessageHandlers;
+
+namespace WebApiOwinSelfHostReverseProxy
+{
+    class Program
+    {
+        static readonly AutoResetEvent WaitEvent = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Console.CancelKeyPress += ConsoleOnCancelKeyPress;
+
+            var options = new StartOptions(args[0]);
+
+            var client = new HttpClient(new HttpClientHandler {UseCookies = false}, true);
+
+            using (WebApp.Start(options, builder =>
+            {
+                var configuration = new HttpConfiguration(); 
+
+                configuration.Routes.MapHttpRoute(
+                    "Proxy",
+                    "{*path}",
+                    new
+                    {
+                        path = RouteParameter.Optional
+                    },
+                    null,
+                    new ProxyDelegatingHandler(args[1], client)
+                );
+
+                builder.UseWebApi(configuration);
+            })) 
+            {
+                Console.WriteLine("Now listening on: {0}", args[0]);
+                Console.WriteLine("Pass to: {0}", args[1]);
+                Console.WriteLine("Application started. Press Ctrl+C to shut down.");
+                Console.WriteLine();
+
+                WaitEvent.WaitOne();
+            }
+        }
+
+        static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            WaitEvent.Set();
+
+            e.Cancel = true;
+        }
+    }
+}
