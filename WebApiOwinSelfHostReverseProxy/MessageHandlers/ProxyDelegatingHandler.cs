9f46b2dce0c61459e8408c4035bc4a001208b16c
Raphael Basso <arabasso@yahoo.com.br>
2020-06-09 14:37:00 -0300

    Added cookie managment support.

--- WebApiOwinSelfHostReverseProxy/MessageHandlers/ProxyDelegatingHandler.cs
+++ WebApiOwinSelfHostReverseProxy/MessageHandlers/ProxyDelegatingHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,13 +10,15 @@ namespace WebApiOwinSelfHostReverseProxy.MessageHandlers
             DelegatingHandler
     {
         private readonly HttpClient _client;
+        private readonly CookieContainer _cookieContainer;
         private readonly Uri _uri;
 
-        public ProxyDelegatingHandler(
-            string baseAddress,
-            HttpClient client)
+        public ProxyDelegatingHandler(string baseAddress,
+            HttpClient client, CookieContainer cookieContainer)
         {
             _client = client;
+            _cookieContainer = cookieContainer;
+
             _uri = new Uri(baseAddress);
         }
 
@@ -23,24 +26,24 @@ namespace WebApiOwinSelfHostReverseProxy.MessageHandlers
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
+            request.RequestUri = new Uri(_uri, request.RequestUri.PathAndQuery);
+
             var context = request.GetOwinContext();
-
-            request.RequestUri = new Uri(_uri, request.RequestUri.LocalPath);
-
-            Console.WriteLine(request.RequestUri);
 
             request.Headers.Add("X-Forwarded-For", context.Request.RemoteIpAddress);
             request.Headers.Add("X-Real-IP", context.Request.RemoteIpAddress);
             request.Headers.Add("X-Forwarded-Proto", _uri.Scheme);
 
             request.Headers.Host = _uri.Host;
+            request.Headers.Remove("Origin");
+            request.Headers.Add("Origin", _uri.OriginalString);
 
             if (request.Method == HttpMethod.Get)
             {
                 request.Content = null;
             }
 
-            return await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+            return await _client.SendAsync(request, cancellationToken).ConfigureAwait(false);
         }
 
         protected override void Dispose(bool disposing)
--- WebApiOwinSelfHostReverseProxy/Program.cs
+++ WebApiOwinSelfHostReverseProxy/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
 using System.Web.Http;
 using Microsoft.Owin.Hosting;
@@ -21,11 +24,17 @@ namespace WebApiOwinSelfHostReverseProxy
 
             var options = new StartOptions(args[0]);
 
-            var client = new HttpClient(new HttpClientHandler {UseCookies = false}, true);
+            var cookieContainer = ReadCookiesFromDisk("Cookies.db");
+
+            var client = new HttpClient(new HttpClientHandler
+            {
+                UseCookies = true,
+                CookieContainer = cookieContainer
+            }, true);
 
             using (WebApp.Start(options, builder =>
             {
-                var configuration = new HttpConfiguration(); 
+                var configuration = new HttpConfiguration();
 
                 configuration.Routes.MapHttpRoute(
                     "Proxy",
@@ -35,11 +44,11 @@ namespace WebApiOwinSelfHostReverseProxy
                         path = RouteParameter.Optional
                     },
                     null,
-                    new ProxyDelegatingHandler(args[1], client)
+                    new ProxyDelegatingHandler(args[1], client, cookieContainer)
                 );
 
                 builder.UseWebApi(configuration);
-            })) 
+            }))
             {
                 Console.WriteLine("Now listening on: {0}", args[0]);
                 Console.WriteLine("Pass to: {0}", args[1]);
@@ -48,6 +57,8 @@ namespace WebApiOwinSelfHostReverseProxy
 
                 WaitEvent.WaitOne();
             }
+
+            WriteCookiesToDisk("Cookies.db", cookieContainer);
         }
 
         static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
@@ -55,6 +66,63 @@ namespace WebApiOwinSelfHostReverseProxy
             WaitEvent.Set();
 
             e.Cancel = true;
+        }
+
+        static void WriteCookiesToDisk(
+            string file,
+            CookieContainer cookieJar)
+        {
+            var appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Assembly.GetExecutingAssembly().GetName().Name);
+
+            Directory.CreateDirectory(appData);
+
+            using (Stream stream = File.Create(Path.Combine(appData, file)))
+            {
+                try
+                {
+                    Console.Out.Write("Writing cookies to disk... ");
+
+                    var formatter = new BinaryFormatter();
+
+                    formatter.Serialize(stream, cookieJar);
+
+                    Console.Out.WriteLine("Done.");
+                }
+
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine("Problem writing cookies to disk: " + e.GetType());
+                }
+            }
+        }
+
+        static CookieContainer ReadCookiesFromDisk(
+            string file)
+        {
+            try
+            {
+                var appData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Assembly.GetExecutingAssembly().GetName().Name);
+
+                Directory.CreateDirectory(appData);
+
+                using (Stream stream = File.Open(Path.Combine(appData, file), FileMode.Open))
+                {
+                    Console.Out.Write("Reading cookies from disk... ");
+
+                    var formatter = new BinaryFormatter();
+
+                    Console.Out.WriteLine("Done.");
+
+                    return (CookieContainer)formatter.Deserialize(stream);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("Problem reading cookies from disk: " + e.GetType());
+
+                return new CookieContainer();
+            }
         }
     }
 }
